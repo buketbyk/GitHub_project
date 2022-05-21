f0d141ffdbe658e414e69e6cf352f9e4f78717a9
buketbyk <bukettbayik@gmail.com>
2022-05-21 18:00:51 +0300

    first message

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(canvasColor: Colors.purpleAccent),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "hi",
+            style: TextStyle(fontSize: 35),
+          ),
+        ),
+      ),
+    );
+  }
+}
